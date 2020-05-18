e5f9e9c72acd46bc9763923514896ba727fed067
Raúl Alarcón López <raulalarcon@MacBook-Pro-de-Raul.local>
2020-05-18 13:22:37 +0200

    Boton

--- express-app/public/redbullf1shop/views/controller/index.js
+++ express-app/public/redbullf1shop/views/controller/index.js
@@ -13,3 +13,15 @@ Controller.controllers.index.goToSignin_clicked = function (event) {
   event.preventDefault();
   Controller.router.go(event.target.href);
 }
+Controller.controllers.addProduct={};
+Controller.controllers.addProduct.addProduct_clicked = function (event, pid) {
+  console.log('Lego')
+  event.preventDefault();
+  Model.buy(pid)
+ .then(function () {
+  console.log('Product added successfully');
+  })
+  .catch(function (err) {
+  console.error('Product cannot be added', err.message);
+  })
+  }
